6f19b269eeac5d09c1bf5fbbbabd06827973f253
andrihuber <andrihuber@localhost>
2021-08-18 16:56:33 +0100

    Initial load

---
+++ Snake/src/dev/noire/snake/Snake.java
@@ -0,0 +1,24 @@
+package dev.noire.snake;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+	public static void main(String[] args) {
+		new Snake();
+	}
+	
+	public Snake() {
+		this.setTitle("Snake Demo");
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new GridLayout(1, 1, 0, 0));
+		
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+}
